71a1e904c8946956b89ad9f6f0d47a6f2cd0df50
juicyaloe <mink4661@snu.ac.kr>
2022-08-29 01:47:37 +0900

    간단 수정

--- backend/macconfig/settings.py
+++ backend/macconfig/settings.py
@@ -132,6 +132,6 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ORIGIN_WHITELIST = (
-    'localhost:3000/'
-)
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]
